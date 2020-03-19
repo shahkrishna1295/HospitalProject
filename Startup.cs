2cca6c4fd6caf9460c81407da74b82d6ba474526
shahkrishna1295 <shahkrishna1295@gmail.com>
2020-03-19 17:20:23 -0400

    first commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HospitalProject.Startup))]
+namespace HospitalProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
