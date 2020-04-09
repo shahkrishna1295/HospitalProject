bd3d5d2a4d861b1b73616539f2a417006b0cef03
ella.qi <ella.qishuyan@gmail.com>
2020-04-09 18:16:54 -0400

    unknown changes again

---
+++ Models/RespondWay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalProject.Models
+{
+    public class RespondWay
+    {
+        /* RespondWay is describing the way of replying the users who submit contact forms
+         * Some things can describe a RespondWay:
+         * RespondWay:
+         *      - Email
+         *      - Call
+         *      - SMS
+         */
+        public int RespondWayID { get; set; }
+    }
+}
