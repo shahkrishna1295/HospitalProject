282e63e3940f7cc9067420bba67b1211d107af38
ella.qi <ella.qishuyan@gmail.com>
2020-04-09 18:46:46 -0400

    update

--- Data/HospitalProjectContext.cs
+++ Data/HospitalProjectContext.cs
@@ -25,14 +25,12 @@ namespace HospitalProject.Data
         public System.Data.Entity.DbSet<HospitalProject.Models.BranchModel> Branch { get; set; } 
         public System.Data.Entity.DbSet<HospitalProject.Models.ServiceModel> Service { get; set; }
 
-<<<<<<< HEAD
         public System.Data.Entity.DbSet<HospitalProject.Models.JobModel> Job { get; set; }
-=======
+
         //contact form feature
         public System.Data.Entity.DbSet<HospitalProject.Models.ContactForm> ContactForms { get; set; }
         public System.Data.Entity.DbSet<HospitalProject.Models.ContactStatus> ContactStatuses { get; set; }
         public System.Data.Entity.DbSet<HospitalProject.Models.RespondWay> RespondWays { get; set; }
->>>>>>> contactform CRUD
 
     }
 }
